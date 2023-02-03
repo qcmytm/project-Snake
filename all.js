5ed6eaa445de30a3167b71658c7250e6a92bf8fa
Q <qcmytm@gmail.com>
2023-02-04 00:58:12 +0800

    3 commit

--- all.js
+++ all.js
@@ -1,34 +1,41 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+// getContext() method會回傳一個canvas的drawing context
 const unit = 20;
 const row = canvas.height / unit;
 const column = canvas.width / unit;
 
 let snake = [];
+createSnake();
+function createSnake() {
+  // 物件的工作是，儲存 蛇身體的x, y座標
+  snake[0] = {
+    x: 80,
+    y: 0,
+  };
+  snake[1] = {
+    x: 60,
+    y: 0,
+  };
+  snake[2] = {
+    x: 40,
+    y: 0,
+  };
+  snake[3] = {
+    x: 20,
+    y: 0,
+  };
+}
 
-snake[0] = {
-  x: 80,
-  y: 0,
-};
-snake[1] = {
-  x: 60,
-  y: 0,
-};
-snake[2] = {
-  x: 40,
-  y: 0,
-};
-snake[3] = {
-  x: 20,
-  y: 0,
-};
 let score = 0;
 let highestScore;
 loadHighestScore();
 document.getElementById("myScore").innerHTML = "遊戲分數:" + score;
 document.getElementById("myScore2").innerHTML = "最高分數:" + highestScore;
+
 class Fruit {
   constructor() {
+    //0~16隨機整數*unit 製作果實座標
     this.x = Math.floor(Math.random() * column) * unit;
     this.y = Math.floor(Math.random() * row) * unit;
   }
@@ -37,11 +44,12 @@ class Fruit {
     ctx.fillStyle = "yellow";
     ctx.fillRect(this.x, this.y, unit, unit);
   }
+  //果實被吃掉後,遊戲框內產生隨機亂數座標(確認不予蛇身重疊)生成果實
   pickALocation() {
     let overlapping = false;
     let new_x;
     let new_y;
-
+    //確認是否蛇身重疊loop function
     function checkOverlap(new_x, new_y) {
       for (let i = 0; i < snake.length; i++) {
         if (new_x == snake[i].x && new_y == snake[i].y) {
@@ -52,7 +60,7 @@ class Fruit {
         }
       }
     }
-
+    //與蛇身重疊會再生成隨機變數,直至無重疊確認新果實座標
     do {
       new_x = Math.floor(Math.random() * column) * unit;
       new_y = Math.floor(Math.random() * row) * unit;
@@ -62,11 +70,12 @@ class Fruit {
     this.y = new_y;
   }
 }
-
+//果實
 let myFruit = new Fruit();
 
-window.addEventListener("keydown", changeDirection);
 let d = "Right";
+
+window.addEventListener("keydown", changeDirection);
 
 function changeDirection(e) {
   if (e.key == "ArrowRight" && d != "Left") {
@@ -78,11 +87,14 @@ function changeDirection(e) {
   } else if (e.key == "ArrowUp" && d != "Down") {
     d = "Up";
   }
-
+  // 每次按下上下左右鍵之後，在下一幀被畫出來之前，
+  // 不接受任何keydown事件
+  // 這樣可以防止連續按鍵導致蛇在邏輯上自殺
   window.removeEventListener("keydown", changeDirection);
 }
 
 function draw() {
+  // 每次畫圖之前，確認蛇有沒有咬到自己
   for (let i = 1; i < snake.length; i++) {
     if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
       clearInterval(myGame);
@@ -90,12 +102,12 @@ function draw() {
       return;
     }
   }
-
+  // 畫出背景,設定為黑色
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+  // 畫出果實
   myFruit.drawFruit();
-
+  // 畫出蛇身
   for (let i = 0; i < snake.length; i++) {
     if (i == 0) {
       ctx.fillStyle = "lightgreen";
@@ -103,7 +115,7 @@ function draw() {
       ctx.fillStyle = "lightblue";
     }
     ctx.strokeStyle = "white";
-
+    //蛇 -穿牆功能
     if (snake[i].x >= canvas.width) {
       snake[i].x = 0;
     }
@@ -119,7 +131,7 @@ function draw() {
     ctx.fillRect(snake[i].x, snake[i].y, unit, unit);
     ctx.strokeRect(snake[i].x, snake[i].y, unit, unit);
   }
-
+  //蛇依 轉向 d變數方位 而持續推進,搭配轉向不同d變數不同推法
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
   if (d == "Left") {
@@ -131,13 +143,16 @@ function draw() {
   } else if (d == "Right") {
     snakeX += unit;
   }
-
+  //產出 新蛇頭
   let newHead = {
     x: snakeX,
     y: snakeY,
   };
-
+  //確認原本的蛇頭是否有吃到果實,
+  //吃到果實加一格身體(且分數加1分)可以不用arr.pop()蛇尾,
+  //沒吃到果實則arr.pop()刪蛇尾一格,保持原先蛇身
   if (snake[0].x == myFruit.x && snake[0].y == myFruit.y) {
+    //挑地點隨機產生果實
     myFruit.pickALocation();
     score++;
     setHighestScore(score);
@@ -146,12 +161,13 @@ function draw() {
   } else {
     snake.pop();
   }
-
+  //arr 加入新蛇頭
   snake.unshift(newHead);
   window.addEventListener("keydown", changeDirection);
 }
 
-let myGame = setInterval(draw, 100);
+let myGame = setInterval(draw, 130);
+
 function loadHighestScore() {
   if (localStorage.getItem("highestScore") == null) {
     highestScore = 0;
